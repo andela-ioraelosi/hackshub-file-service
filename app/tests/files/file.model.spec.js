d9023221f335a0c8ab854ced384a45c59736d235
Ifeanyi Oraelosi <ifeanyi.oraelosi@andela.co>
2015-04-05 21:00:33 +0100

    Add test specs for file model.
    + Add test spec to validate the creation of new file objects.
    + Add test spec to validate updates on file objects.
    + Add test spec to validate the removal of file objects.

--- app/tests/files/file.model.spec.js
+++ app/tests/files/file.model.spec.js
@@ -21,11 +21,11 @@ Globals
 var file;
 describe('File Model Unit Tests: ', function () {
 
-  describe('Method Save', function () {
+  describe('Create File Objects', function () {
 
     beforeEach(function (done) {
       file = new FileModel({
-        name: 'A random file',
+        filename: 'A random file',
         created_at: new Date()
       });
       done();
@@ -38,12 +38,87 @@ describe('File Model Unit Tests: ', function () {
       });
     });
 
-    it('should throw an error when saved without a name', function (done) {
-      file.name = '';
+    it('should throw an error when saved without a filename', function (done) {
+      file.filename = '';
 
       file.save(function (err, file) {
         expect(err).not.toBeNull();
         done();
+      });
+    });
+
+    it('should create a file object', function (done) {
+      file.filename = "image.jpg";
+
+      file.save(function (err, file) {
+        FileModel.find({filename: "image.jpg"}, function (err, file) {
+          expect(err).toBeNull();
+        });
+      });
+    });
+
+    afterEach(function (done) {
+      file.remove(function (err, file) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      done();
+    });
+
+  });
+
+  describe('Edit File Objects: ', function () {
+
+    beforeEach(function (done) {
+      file = new FileModel({
+        filename: "funny.gif",
+        created_at: new Date()
+      });
+      done();
+    });
+
+    it('should update the file object', function () {
+      file.save(function (err, file) {
+        FileModel.find({filename: "funny.gif"}, function (err, file) {
+          file.filename = "not_funny.gif";
+
+          file.save(function (err, file) {
+            FileModel.find({filename: "not_funny.gif"}, function (err, file) {
+              expect(err).toBeNull();
+            });
+          });
+        });
+      });
+    });
+
+    afterEach(function (done) {
+      file.remove(function (err, file) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      done();
+    });
+  });
+
+  describe('Delete File Objects: ', function () {
+
+    beforeEach(function (done) {
+      file = new FileModel({
+        filename: "funny.gif",
+        created_at: new Date()
+      });
+      done();
+    });
+
+    it('should delete the file object', function () {
+      file.save(function (err, file) {
+        file.remove(function (err, file) {
+          FileModel.find({filename: "funny.gif"}, function (err, file) {
+            expect(file).toBeNull();
+          });
+        });
       });
     });
 
