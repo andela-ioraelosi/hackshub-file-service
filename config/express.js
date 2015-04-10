40f31a6ae521db880ce850a2a10d8d8d41564c73
Ifeanyi <ifeanyi.oraelosi@andela.co>
2015-04-10 15:13:54 +0100

    Integrate Dropbox module.
    + Create client for Dropbox.
    + Authenticate client.
    + Export Dropbox client to API routes.

--- config/express.js
+++ config/express.js
@@ -6,9 +6,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 
-var router = require('./../app/files/routes')();
+var dropbox = require("dropbox");
+var client = new dropbox.Client({
+    key: "df6n0p9kq2fd7o1",
+    secret: "2yxvgwwldx5isf0"
+});
+client.authDriver(new dropbox.AuthDriver.NodeServer(8191));
+var dropboxClient = client.authenticate(function(error, client) {
+  if (error) {
+    console.error(error);
+  }
+});
 
-module.exports = function () {
+module.exports = function (dropboxClient) {
 
   var app = express();
 
@@ -20,6 +30,8 @@ module.exports = function () {
   app.use(bodyParser.urlencoded({
     extended: true
   }));
+
+  var router = require('./../app/files/routes')(dropboxClient);
 
   app.use(bodyParser.json());
   app.use('/api/v1/', router);
