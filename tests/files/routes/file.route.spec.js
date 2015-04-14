56245264868c854b075328529c0b78c8626feac2
Ifeanyi <ifeanyi.oraelosi@andela.co>
2015-04-14 16:27:09 +0100

    Create specs for testing API routes.
    + Fix references to config file.
    + Fix references to File Model.
    + Create spec to test POST to /files endpoint.
    + Create spec to test PUT to /files/:path endpoint.
    + Create spec to test DELETE to /files/:path endpoint.
    + Establish connection to MongoDB database.

--- tests/files/routes/file.route.spec.js
+++ tests/files/routes/file.route.spec.js
@@ -1,97 +1,175 @@
 'use strict';
 
-var app = require('./../config/express')();
-var config = require('./../config/config')();
+var app = require('./../../../config/express')();
+var config = require('./../../../config/config')();
 var mongoose = require('mongoose');
 var request = require('supertest');
-var FileModel = require('./../app/files/models/file.model');
+var FileModel = require('./../../../app/files/models/file.model');
+
+mongoose.connect(config.db[process.env.NODE_ENV].uri, config.db[process.env.NODE_ENV].options, function (err) {
+  if (err) {
+    console.error('Could not connect to MongoDB.');
+    console.log(err);
+  }
+});
 
 describe('File API endpoints: ', function () {
 
-  beforeEach(function (done) {
-
-    function clearDB () {
-      for (var i = 0; i < mongoose.connection.collections.length; i++) {
-        mongoose.connection.collections[i].remove();
-      }
-
-      return done();
-    }
-
-    function reconnect () {
-      mongoose.connect(config.db[process.env.NODE_ENV].uri, function (err) {
-        if (err) {
-          throw err;
-        }
-
-        return clearDB();
-      });
-    }
-
-    function checkState() {
-      switch(mongoose.connection.readyState) {
-        case 0:
-          reconnect();
-          break;
-        case 1:
-          clearDB();
-          break;
-        default:
-          process.nextTick(checkState);
-      }
-    }
-
-    checkState();
-  });
-
-  describe('GET /api/v1/files', function () {
-
-    it('should respond with json', function (done) {
-
-      request(app)
-        .get('/api/v1/files')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end(function (err, response) {
-          expect(err).toBeNull();
-          expect(response.body).toBeDefined();
-          done();
-        });
-    });
-
-  });
-
   describe('POST /api/v1/files', function () {
 
-    var post_data = null;
-
-    beforeEach(function () {
-      post_data = {
-        filename: "happy.jpg",
-        created_at: new Date()
-      };
+    beforeEach(function (done) {
+      FileModel.remove({},function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      done();
     });
 
     it('should add a new file', function (done) {
 
       request(app)
         .post('/api/v1/files')
-        .send(post_data)
+        .attach('fileName', 'test_files/avatar.png','avatar.png')
         .expect(200)
         .end(function (err, response) {
-          expect(err).toBeNull();
-          FileModel.findOne(post_data, function (err, file) {
+          FileModel.findOne({ path: /avatar/ }, function (err, file) {
             expect(file).not.toBeNull();
           });
           done();
         });
     });
 
+    afterEach(function (done) {
+      FileModel.remove({},function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      done();
+    });
+
   });
 
-  afterEach(function (done) {
-    mongoose.disconnect();
-    return done();
+  describe('GET /api/v1/files', function () {
+
+    beforeEach(function (done) {
+
+      request(app)
+        .post('/api/v1/files')
+        .attach('fileName', 'test_files/avatar.png','avatar.png')
+        .attach('fileName', 'test_files/avatar.png','avatar.png')
+        .attach('fileName', 'test_files/avatar.png','avatar.png')
+        .attach('fileName', 'test_files/avatar.png','avatar.png')
+        .expect(200)
+        .end(function (err, response) {
+          if (err) {
+            console.log(err);
+          }
+          done();
+        });
+
+    });
+
+    it('should respond with an array of file objects', function (done) {
+
+      request(app)
+        .get('/api/v1/files')
+        .expect(200)
+        .end(function (err, response) {
+          expect(response.body.length).toBeGreaterThan(0);
+          done();
+        });
+    });
+
+    afterEach(function (done) {
+      FileModel.remove({},function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      done();
+    });
+
+  });
+
+  describe('PUT /api/v1/files/:path', function () {
+
+    beforeEach(function (done) {
+
+      request(app)
+        .post('/api/v1/files')
+        .attach('fileName', 'test_files/hello.txt','hello.txt')
+        .expect(200)
+        .end(function (err, response) {
+          if (err) {
+            console.log(err);
+          }
+          done();
+        });
+
+    });
+
+    it('should edit a file.', function (done) {
+
+      request(app)
+        .put('/api/v1/files/hello.txt')
+        .attach('fileName', 'test_files/goodbye.txt','hello.txt')
+        .expect(200)
+        .end(function (err, response) {
+          expect(response.body.message).toEqual('File updated successfully.');
+          done();
+        });
+    });
+
+    afterEach(function (done) {
+      FileModel.remove({},function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      done();
+    });
+
+  });
+
+  describe('DELETE /api/v1/files/:path', function () {
+
+    beforeEach(function (done) {
+
+      request(app)
+        .post('/api/v1/files')
+        .attach('fileName', 'test_files/hello.txt','hello.txt')
+        .expect(200)
+        .end(function (err, response) {
+          if (err) {
+            console.log(err);
+          }
+          done();
+        });
+
+    });
+
+    it('should edit a file.', function (done) {
+
+      request(app)
+        .delete('/api/v1/files/hello.txt')
+        .expect(200)
+        .end(function (err, response) {
+          expect(response.body.message).toEqual('File deleted successfully.');
+          done();
+        });
+    });
+
+    afterEach(function (done) {
+      FileModel.remove({},function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      done();
+    });
+
   });
 
 });
